bbf9e1e67e316ed63651af5eacd6b239df6476a2
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-24 18:17:40 -0300

    feat: create room models

--- projeto/apps/room/models.py
+++ projeto/apps/room/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    capacity = models.IntegerField()
+    projection_type = models.CharField(max_length=10)
+    accessibility = models.BooleanField(default=False)
+    cinema = models.ForeignKey('cinema.Cinema', on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'room'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"Room {self.id}"
