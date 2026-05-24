5690f417e7ca27cf6bd7121ba7642489bce05c8b
gfreitas15 <gfmonguilhott793@gmail.com>
2026-05-24 18:44:53 -0300

    feat: create client forms, models and admin.py

--- projeto/apps/client/models.py
+++ projeto/apps/client/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from apps.person.models import Person
 
 # Create your models here.
+class Client(Person):
+    gender = models.CharField(max_length=1, choices=[
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    ])   
+    cpf = models.CharField(max_length=11)
+    date_of_birth = models.DateField()
+
+    class Meta:
+        db_table = 'client'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
