6cf9569c7c312ffff53d1623ee1132dae5665ade
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-24 14:57:46 -0300

    fix: fix views reference on urls.py

--- projeto/apps/cinema/urls.py
+++ projeto/apps/cinema/urls.py
@@ -5,7 +5,7 @@ app_name = 'cinema'
 
 urlpatterns = [
     path('', views.cinema_list, name='cinema_list'),
-    path('create/', views.cinema_create, name='add_cinema'),
-    path('<int:pk>/edit/', views.cinema_edit, name='edit_cinema'),
-    path('<int:pk>/delete/', views.cinema_delete, name='delete_cinema'),
+    path('create/', views.add_cinema, name='add_cinema'),
+    path('<int:pk>/edit/', views.edit_cinema, name='edit_cinema'),
+    path('<int:pk>/delete/', views.delete_cinema, name='delete_cinema'),
 ]
