c043c3275602ef7b3fe9baaa53d3bab25d1c3d12
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-24 20:51:30 -0300

    fix: handle urlpatterns

--- projeto/apps/room/urls.py
+++ projeto/apps/room/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('cinema/<int:cinema_id>/rooms/', views.room_list, name
             ='room_list'),
     path('cinema/<int:cinema_id>/rooms/add/', views.add_room, name='add_room'),
-    path('rooms/<int:pk>/edit/', views.edit_room, name='edit_room'),
-    path('rooms/<int:pk>/delete/', views.delete_room, name='delete_room
-    ),
+    path('cinema/<int:cinema_id>/rooms/<int:pk>/edit/', views.edit_room, name='edit_room'),
+    path('cinema/<int:cinema_id>/rooms/<int:pk>/delete/', views.delete_room, name='delete_room'),
 ]
