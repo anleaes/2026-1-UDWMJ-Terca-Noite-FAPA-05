b37e205a382c7b5627ab0aa90dacdb45de90cfee
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-24 20:36:29 -0300

    fix: room views update

--- projeto/apps/room/views.py
+++ projeto/apps/room/views.py
@@ -2,49 +2,63 @@ from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import RoomForm
 from .models import Room
+from cinema.models import Cinema
 # Create your views here.
 def add_room(request, cinema_id):
     template_name = 'room/add_room.html'
-    context = {}
+    cinema = get_object_or_404(
+        Cinema,
+        id=cinema_id
+    )
 
     if request.method == 'POST':
         form = RoomForm(request.POST)
 
         if form.is_valid():
             room = form.save(commit=False)
-            room.cinema_id = cinema_id
+            room.cinema = cinema
             room.save()
-            return redirect('cinema:cinema_list')
+            return redirect('cinema:edit_cinema', cinema.id)
     else:
         form = RoomForm()
 
-    context['form'] = form
-    return render(request, template_name, context)
+    context = {
+        'form': form,
+        'cinema': cinema
+    }
 
-def edit_room(request, pk):
+    return render(
+        request,
+        template_name,
+        context
+    )
+
+def edit_room(request, cinema_id, pk):
     template_name = 'room/add_room.html'
     context = {}
-    room = get_object_or_404(Room, pk=pk)
+    room = get_object_or_404(Room, id=pk, cinema_id=cinema_id)
 
     if request.method == 'POST':
         form = RoomForm(request.POST, instance=room)
 
         if form.is_valid():
             form.save()
-            return redirect('cinema:cinema_list')
+            return redirect('cinema:edit_cinema', room.cinema.id)
     else:
         form = RoomForm(instance=room)
 
-    context['form'] = form
+    context = {
+        'form': form,
+        'cinema': room.cinema,
+        'room': room
+    }
     return render(request, template_name, context)
 
-def delete_room(request, pk):
-    room = get_object_or_404(Room, pk=pk)
-    if request.method == 'POST':
-        room.delete()
-        return redirect('cinema:cinema_list')
-    return render(request, 'room/confirm_delete.html', {'room': room})
-
+def delete_room(request, cinema_id, pk):
+    room = get_object_or_404(Room, id=pk, cinema_id=cinema_id)
+    room.delete()
+    return redirect('cinema:edit_cinema', cinema_id)
+  
 def room_list(request, cinema_id):
     template_name = 'room/room_list.html'
     rooms = Room.objects.filter(cinema_id=cinema_id)
