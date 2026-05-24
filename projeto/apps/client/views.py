b1e016a7ef0accc6ebe8f0e7123a239869c9b6b6
gfreitas15 <gfmonguilhott793@gmail.com>
2026-05-24 20:07:11 -0300

    fix: fix ClientForm import

--- projeto/apps/client/views.py
+++ projeto/apps/client/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
-from django.shortcuts import render
+from .forms import ClientForm
 from .models import Client
 
 # Create your views here.
