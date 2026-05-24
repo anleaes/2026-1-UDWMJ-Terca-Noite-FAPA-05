f0a33f9402fe325cfdddf4d68a1ab4881bc93d6b
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-24 18:41:06 -0300

    feat: create urls for room

---
+++ projeto/apps/room/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'room'
+
+urlpatterns = [
+    path('cinema/<int:cinema_id>/rooms/', views.room_list, name
+            ='room_list'),
+    path('cinema/<int:cinema_id>/rooms/add/', views.add_room, name='add_room'),
+    path('rooms/<int:pk>/edit/', views.edit_room, name='edit_room'),
+    path('rooms/<int:pk>/delete/', views.delete_room, name='delete_room
+    ),
+]
