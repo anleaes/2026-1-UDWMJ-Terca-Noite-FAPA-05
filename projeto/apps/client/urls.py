ff61f0456b3225df9862a876096b5e68a771a392
gfreitas15 <gfmonguilhott793@gmail.com>
2026-05-24 20:06:01 -0300

    fix: add correct pk

--- projeto/apps/client/urls.py
+++ projeto/apps/client/urls.py
@@ -6,6 +6,6 @@ app_name = 'client'
 urlpatterns = [
     path('', views.client_list, name='client_list'),
     path('add/', views.add_client, name='add_client'),
-    path('edit/<int:client_id>/', views.edit_client, name='edit_client'),
-    path('delete/<int:client_id>/', views.delete_client, name='delete_client'),
+    path('edit/<int:pk>/', views.edit_client, name='edit_client'),
+    path('delete/<int:pk>/', views.delete_client, name='delete_client'),
 ]
