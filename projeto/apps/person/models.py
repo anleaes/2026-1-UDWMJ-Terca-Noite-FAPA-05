c6a0ada492e5ef3cfa24e1d0b31b56dfda491cfc
gfreitas15 <gfmonguilhott793@gmail.com>
2026-05-24 18:08:28 -0300

    feat: create person models.py

--- projeto/apps/person/models.py
+++ projeto/apps/person/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    cnpj = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'person'
+        ordering = ['id']
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
