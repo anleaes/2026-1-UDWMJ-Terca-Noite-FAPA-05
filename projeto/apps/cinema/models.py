f99adf6718c36c99cdf066a913418abe1f1d0c35
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-23 22:03:28 -0300

    feat: create cinema models

--- projeto/apps/cinema/models.py
+++ projeto/apps/cinema/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Cinema(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    phone = models.CharField(max_length=20)
+    cnpj = models.CharField(max_length=20)
+
+    class Meta:
+        db_table = 'cinemas'
+        ordering = ['name']
+
+
+    def __str__(self):
+        return self.name
