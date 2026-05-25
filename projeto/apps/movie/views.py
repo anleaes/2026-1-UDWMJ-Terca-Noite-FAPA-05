fde85f3db0265f78b2b041a05dc8b2a4f182b078
VictoryMSX <vpieluhowski@gmail.com>
2026-05-24 22:40:18 -0300

    feat: create movie views

--- projeto/apps/movie/views.py
+++ projeto/apps/movie/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from urllib import request
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Movie 
+from .forms import MovieForm
 
 # Create your views here.
+
+def add_movie(request):
+    template_name = 'movie/add_movie.html'
+    context = {}
+
+    if request.method == 'POST':
+        form = MovieForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('movie:movie_list')
+    else:
+        form = MovieForm()
+
+    context['form'] = form
+
+    return render(request, template_name, context)
+
+def movie_list(request):
+    template_name = 'movie/movie_list.html'
+    context = {}
+
+    movies = Movie.objects.all()
+    context['movies'] = movies
+
+    return render(request, template_name, context)
+
+def edit_movie(request, movie_id):
+    template_name = 'movie/add_movie.html'
+    context = {}
+
+    movie = get_object_or_404(Movie, pk=movie_id)
+
+    if request.method == 'POST':
+        form = MovieForm(
+            request.POST,
+            request.FILES,
+            instance=movie
+        )
+
+        if form.is_valid():
+            form.save()
+            return redirect('movie:movie_list')
+    else:
+        form = MovieForm(instance=movie)
+
+    context['form'] = form
+
+    return render(request, template_name, context)
+
+
+def delete_movie(request, movie_id):
+    movie = get_object_or_404(Movie, pk=movie_id)
+
+    movie.delete()
+
+    return redirect('movie:movie_list')
