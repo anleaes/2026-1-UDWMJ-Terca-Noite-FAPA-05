ec6bdc617e68231e44b648a0135e163e2db57be0
VictoryMSX <vpieluhowski@gmail.com>
2026-05-24 22:31:04 -0300

    feat: create movies models forms and admin.py

--- projeto/apps/movie/models.py
+++ projeto/apps/movie/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from genre.models import Genre
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    synopsis = models.TextField()
+    duration_minutes = models.IntegerField()
+    age_rating = models.IntegerField(max_length=3)
+    poster = models.ImageField(
+        upload_to='movie_posters/', 
+        null=True, 
+        blank=True
+    )
+    genres = models.ManyToManyField(   
+        'Genre', 
+        related_name='movies'
+    )
+
+    class Meta:
+        db_table = 'movie'
+        ordering = ['id']
+
+
+    def __str__(self):
+        return f"{self.title}"
