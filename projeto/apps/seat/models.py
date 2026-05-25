d189d9b2bf79827cacd400936d6dd278621a955d
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-24 23:21:04 -0300

    feat: create seat models and add to admin

--- projeto/apps/seat/models.py
+++ projeto/apps/seat/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Seat(models.Model):
+    row = models.CharField(max_length=5)
+    number = models.IntegerField()
+
+    room = models.ForeignKey(
+        'room.Room',
+        on_delete=models.CASCADE,
+        related_name='seats'
+    )
+
+    class Meta:
+        db_table = 'seats'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"Seat {self.row}{self.number} in Room {self.room.id} is {self.status}"
+
+    def generate_seats(room):
+
+        if room.seats.exists():
+            return
+        seats = []
+        
+        for r in range(room.rows):
+            row_letter = chr(65 + r) 
+            for n in range(1, room.seats_per_row + 1):
+                seat = Seat(
+                    row=row_letter,
+                    number=n,
+                    room=room
+                )
+                seats.append(seat)
+        Seat.objects.bulk_create(seats)
