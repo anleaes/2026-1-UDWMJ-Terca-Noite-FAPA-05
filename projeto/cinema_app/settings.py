15e6df875970e1bb4db9aed78619627856a1d455
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-24 21:10:02 -0300

    feat: adds room to settings and urls

--- projeto/cinema_app/settings.py
+++ projeto/cinema_app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'cinema.apps.CinemaConfig',
     'client.apps.ClientConfig',
     'person.apps.PersonConfig',
+    'room.apps.RoomConfig',
 ]
 
 MIDDLEWARE = [
--- projeto/cinema_app/urls.py
+++ projeto/cinema_app/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('cinema/', include('cinema.urls', namespace='cinema')),
     path('client/', include('client.urls', namespace='client')),
+    path('room/', include('room.urls', namespace='room')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
