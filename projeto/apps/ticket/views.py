1f76b87c5b8776841acbf2da12e53d0e3552046d
pojasv2 <rafaelsilveiradevargas@gmail.com>
2026-05-26 20:13:47 -0300

    feat: add list ticket

--- projeto/apps/ticket/views.py
+++ projeto/apps/ticket/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+@client_required
+def ticket_list(request, client_id):
+    template_name = 'ticket/ticket_list.html'
+
+    tickets = Ticket.objects.filter(client_id=client_id)
+
+    context = {
+        'tickets': tickets,
+    }
+
+    return render(request, template_name, context)
