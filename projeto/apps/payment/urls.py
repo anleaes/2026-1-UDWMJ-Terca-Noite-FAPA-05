59b7ae0bbe108d6d93b697c47ab791401216d6ff
GFreitas15 <gfmonguilhott793@gmail.com>
2026-05-26 00:03:03 -0300

    feat: create payment urls

---
+++ projeto/apps/payment/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'payment'
+
+urlpatterns = [
+    path('', views.payment_list, name='payment_list'),
+    path('add/', views.add_payment, name='add_payment'),
+    path('edit/<int:pk>/', views.edit_payment, name='edit_payment'),
+    path('delete/<int:pk>/', views.delete_payment, name='delete_payment'),
+]
