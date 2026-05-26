759f2595b6fb1f2c70d89b1b0e56608d15b65e36
GFreitas15 <gfmonguilhott793@gmail.com>
2026-05-25 23:25:02 -0300

    feat: create order app, configure models, forms and admin.py

---
+++ projeto/apps/order/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Order(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=100)
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    client = models.ForeignKey('client.Client', on_delete=models.CASCADE, related_name='orders')
+    employee = models.ForeignKey('employee.Employee', on_delete=models.CASCADE, related_name='orders')
+
+    class Meta:
+        db_table = 'order'
+        ordering = ['id']
+
+    def __str__(self):
+        return f"Order {self.id}"
