188f830dfba239d1467843f15604d126c03e1c9f
VictoryMSX <vpieluhowski@gmail.com>
2026-05-27 19:37:00 -0300

    feat: Remove active from genre-apps and templates

--- projeto/apps/genre/models.py
+++ projeto/apps/genre/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class Genre(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    active = models.BooleanField(default=True)
-
+    
     class Meta:
         db_table = 'genres'
         ordering = ['id']
